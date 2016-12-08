0f7df65d075ce723e154681df5c09c1dc4206c6a
Thales <thalesrocha.ti@gmail.com>
2016-12-08 21:15:11 -0200

    First commit

---
+++ Condo/src/main/java/br/com/haka/CondoApplication.java
@@ -0,0 +1,12 @@
+package br.com.haka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondoApplication.class, args);
+	}
+}
